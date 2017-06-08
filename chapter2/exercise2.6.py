a920dd6b3506a8647468f4afd3e351fccc175acf
ghislaincabrel <ghislaincabrel.kemfang@gmail.com>
2017-06-08 12:00:00 +0100

    exercice of chapter 2

---
+++ chapter2/exercise2.6.py
@@ -0,0 +1,15 @@
+#exercise 2.6
+# program that prints a box ,an oval , an arrow and a dimaond
+print(" the box ")
+
+for i in range(0,5):
+ 	print("*",end= " ")
+
+for i in range(0,10):
+	print("*         *")
+	
+
+for i in range(0,6):
+ 	print("*",end= " ")
+print("\n")
+
